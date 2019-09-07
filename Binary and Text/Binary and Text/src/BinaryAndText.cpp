616d3622912ed0ebfdee850aaa2890885ab66b7a
Matthew Day <Daystar.1998@yahoo.com>
2019-09-07 12:51:25 -0500

    Add main

---
+++ Binary and Text/Binary and Text/src/BinaryAndText.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+	Name: binaryAndText.cpp
+
+	Des:
+		This program will read a binary file and output the data into a text
+		file
+
+	Author: Matthew Day
+
+	Due Date: 9/8/2019
+******************************************************************************/
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
